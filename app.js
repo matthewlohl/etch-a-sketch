7a3552b552de53471da072d48e67ae861975b5a7
Matthew Lo <matthewlol316@gmail.com>
2022-05-29 16:01:31 +0100

    Boxes hovered will be colored

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ document.getElementById("test").addEventListener("click", createGrid);
 
 // Create Square Divs
 function createDiv(size){
-    const div = document.createElement("div");
-    div.classList.add("box")
+    const div = document.createElement('div');
+    div.classList.add('box');
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
     console.log(size);
@@ -22,10 +22,26 @@ function createGrid(){
     //use 2 for loops to create the squares
     for (i = 0; i<8; i++){
         for (j = 0; j<8; j++){
-            grid.appendChild(createDiv(grid.clientWidth/gridSize));
+            grid.appendChild(createDiv(Math.ceil(((grid.clientWidth-0.5*2*gridSize)/gridSize))));
         }
     }
+    console.log((grid.clientWidth/gridSize)-0.5*2*gridSize);
+    console.log((63+0.5*2)*8);
+    console.log(grid.clientWidth);
 }
 
 
-// Turn the square into Black
+
+// ----- Turn Box into Black ---- //
+//Add 'active' class to hovered boxes
+grid.addEventListener('mouseover', function(e){
+    //add active class to the boxes that were hovered
+    if (e.target.matches('.box')){
+        e.target.classList.add('active')
+    }
+    if (e.target.matches('active')){
+        e.target.style.backgroundColor = 'blue';
+    }
+});
+
+
