b06c9aaabc43253f7f2a35c42ee427fe8006b932
Matthew Lo <matthewlol316@gmail.com>
2022-06-03 17:28:13 +0100

    able to change number of boxes using a slider

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
 const grid = document.querySelector('.grid');
-let gridSize = 8;
+const applyGridSize = document.querySelector('.ApplyGrid');
+const sliderValue=document.getElementById('slider');
+let gridSize = 10;
 
-
-// Test function
-document.getElementById("test").addEventListener("click", createDiv);
-document.getElementById("test").addEventListener("click", createGrid);
-
-// Create Square Divs
-function createDiv(size){
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    console.log(size);
-
-    return div;
-}
-
-// Append Div to Grid
+// ---- Create Grids ----
 function createGrid(){
     //use 2 for loops to create the squares
-    for (i = 0; i<8; i++){
-        for (j = 0; j<8; j++){
-            grid.appendChild(createDiv(Math.ceil(((grid.clientWidth-0.5*2*gridSize)/gridSize))));
-        }
+    for (i = 0; i<256; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('mouseover', function(e){
+            // Grab target add .active class
+            e.target.classList.add('active');
+        })
+        grid.appendChild(div);
     }
-    console.log((grid.clientWidth/gridSize)-0.5*2*gridSize);
-    console.log((63+0.5*2)*8);
-    console.log(grid.clientWidth);
+};
+
+// ---- Remove Child from div --- //
+function removeChildGrid(){
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
 }
 
 
-
-// ----- Turn Box into Black ---- //
-//Add 'active' class to hovered boxes
-grid.addEventListener('mouseover', function(e){
-    //add active class to the boxes that were hovered
-    if (e.target.matches('.box')){
-        e.target.classList.add('active')
-    }
-    if (e.target.matches('active')){
-        e.target.style.backgroundColor = 'blue';
+// ----- Get new control value and reapply to Div Child---- //
+//document.getElementById('ApplyGrid').addEventListener('click', reset);
+slider.addEventListener('input',function(){
+    removeChildGrid();
+    console.log(sliderValue.value);
+    grid.setAttribute('style', `grid-template-columns: repeat(${sliderValue.value},2fr); grid-template-rows: repeat(${sliderValue.value},2fr);`);
+    for (i = 0; i <sliderValue.value*sliderValue.value; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('mouseover',function(e){
+            e.target.classList.add('active');
+        })
+        grid.appendChild(div);
     }
 });
 
-
+/*applyGridSize.addEventListener('click', function(){
+    reset ();
+})
+*/
+createGrid();
