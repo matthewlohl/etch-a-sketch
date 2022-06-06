09d4c83374cc3b674f41512c255c10e75615126d
Matthew Lo <matthewlol316@gmail.com>
2022-06-06 23:39:59 +0100

    Added reset button

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const grid = document.querySelector('.grid');
-const applyGridSize = document.querySelector('.ApplyGrid');
+const reset = document.querySelector('.reset');
 const sliderValue=document.getElementById('slider');
 let gridSize = 10;
 
 // ---- Create Grids ----
 function createGrid(){
+
     //use 2 for loops to create the squares
     for (i = 0; i<256; i++){
         const div = document.createElement('div');
@@ -17,6 +18,7 @@ function createGrid(){
     }
 };
 
+
 // ---- Remove Child from div --- //
 function removeChildGrid(){
     while(grid.firstChild){
@@ -26,23 +28,44 @@ function removeChildGrid(){
 
 
 // ----- Get new control value and reapply to Div Child---- //
-//document.getElementById('ApplyGrid').addEventListener('click', reset);
+
+
 slider.addEventListener('input',function(){
     removeChildGrid();
-    console.log(sliderValue.value);
     grid.setAttribute('style', `grid-template-columns: repeat(${sliderValue.value},2fr); grid-template-rows: repeat(${sliderValue.value},2fr);`);
     for (i = 0; i <sliderValue.value*sliderValue.value; i++){
         const div = document.createElement('div');
         div.classList.add('box');
         div.addEventListener('mouseover',function(e){
             e.target.classList.add('active');
-        })
+        });
         grid.appendChild(div);
-    }
+    };
 });
 
-/*applyGridSize.addEventListener('click', function(){
-    reset ();
-})
-*/
+
+reset.addEventListener('click', function(){
+    removeChildGrid();
+    grid.setAttribute('style', `grid-template-columns: repeat(${sliderValue.value},2fr); grid-template-rows: repeat(${sliderValue.value},2fr);`);
+    for (i = 0; i <sliderValue.value*sliderValue.value; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('mouseover',function(e){
+            e.target.classList.add('active');
+        });
+        grid.appendChild(div);
+    };
+});
+
+
+/*reset.addEventListener('click',resetGrid(){
+    console.log(divReset);
+}
+);*/
+
+//function resetGrid(){
+    
+    //divReset.classList.add('reset');
+    //}
+
 createGrid();
